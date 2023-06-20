51b84c0cffe723bdddd3374ec98a2c3a20615b3a
שרה תמר אמיתי <saratama@g.jct.ac.il>
2023-06-20 19:26:18 +0300

    final?

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const mysql = require("mysql2");
+const util = require("util");
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -19,10 +26,15 @@ connection.connect((err) => {
   console.log("Connected to the MySQL server");
 });
 
-// Retrieve all users
+
 app.get("/users", (req, res) => {
-  const query = "SELECT * FROM users";
-  connection.query(query, (err, results) => {
+  let query = "SELECT * FROM users WHERE username = ?";
+  let username = req.query.username;
+  if(!username){
+    username = "a";
+    query = "SELECT * FROM users WHERE 'a' = ?";
+  }
+  connection.query(query, [username], (err, results) => {
     if (err) {
       console.error("Failed to fetch users:", err);
       res.status(500).json({ error: "Failed to fetch users" });
@@ -35,6 +47,7 @@ app.get("/users", (req, res) => {
           password: user.password,
           id: user.user_id,
           name: user.name,
+          city: user.city,
         }))
       );
     }
@@ -49,9 +62,11 @@ app.get("/users/:id", (req, res) => {
     if (err) {
       console.error("Failed to fetch user:", err);
       res.status(500).json({ error: "Failed to fetch user" });
-    } else {
+    } else if (result.length > 0) {
       const { user_id, username, password, email, city, name } = result[0];
       res.json({ id: user_id, username, password, email, city, name });
+    } else {
+      res.status(404).json({ error: "User not found" });
     }
   });
 });
@@ -99,64 +114,51 @@ app.put("/users/:id", (req, res) => {
 });
 
 // Delete a user
-app.delete("/users/:id", (req, res) => {
+app.delete("/users/:id", async (req, res) => {
   const userId = req.params.id;
-  //get all this user's posts 
-  let postsquery = "SELECT * FROM posts WHERE user_id=?";
-  let post_ids=[];
-  connection.query(postsquery, [userId], (err, results) => {
-    if (err) {
-      console.error("Failed to fetch posts:", err);
-      res.status(500).json({ error: "Failed to fetch posts" });
-    } else {
-      results.forEach((post) => {
-      post_ids.push(post.post_id); 
-    });
-    
-    }
-  });
-  //for each post, delete all it's comments
-  for(let i=0; i<post_ids.length; i++){
-    let postId=post_ids[i];
-    const commentQuery="DELETE FROM comments WHERE post_id = ?";
-    connection.query(commentQuery, [postId], (err, result) => {
-    if (err) {
-      console.error("Failed to delete comments:", err);
-      res.status(500).json({ error: "Failed to delete comments" });
-    } else {
-    }
-  });
-  }
-  //delete all posts of this user:
-  const postQuery="DELETE FROM posts WHERE user_id = ?";
-  connection.query(postQuery, [userId], (err, result) => {
-  if (err) {
-      console.error("Failed to delete posts:", err);
-      res.status(500).json({ error: "Failed to delete posts" });
-    } else {
-    }
-  });
-  //delete all todos of this user
-  const todosQuery="DELETE FROM todos WHERE user_id = ?";
-  connection.query(todosQuery, [userId], (err, result) => {
-  if (err) {
-      console.error("Failed to delete todos:", err);
-      res.status(500).json({ error: "Failed to delete todos" });
-    } else {
-    }
-  });
-  //finally, delete the user
-  const query = "DELETE FROM users WHERE user_id = ?";
-  connection.query(query, [userId], (err, result) => {
-    if (err) {
-      console.error("Failed to delete user:", err);
-      res.status(500).json({ error: "Failed to delete user" });
-    } else {
-      res.json({ message: "User deleted successfully" });
-    }
-  });
-});
 
+  try {
+    // Get all this user's posts
+    const postsQuery = "SELECT * FROM posts WHERE user_id=?";
+    const postsResult = await util.promisify(connection.query).bind(connection)(
+      postsQuery,
+      [userId]
+    );
+    const postIds = postsResult.map((post) => post.post_id);
+
+    // Delete all comments for each post
+    for (let i = 0; i < postIds.length; i++) {
+      const postId = postIds[i];
+      const commentQuery = "DELETE FROM comments WHERE post_id = ?";
+      await util.promisify(connection.query).bind(connection)(commentQuery, [
+        postId,
+      ]);
+    }
+
+    // Delete all posts of this user
+    const postQuery = "DELETE FROM posts WHERE user_id = ?";
+    await util.promisify(connection.query).bind(connection)(postQuery, [
+      userId,
+    ]);
+
+    // Delete all todos of this user
+    const todosQuery = "DELETE FROM todos WHERE user_id = ?";
+    await util.promisify(connection.query).bind(connection)(todosQuery, [
+      userId,
+    ]);
+
+    // Finally, delete the user
+    const deleteQuery = "DELETE FROM users WHERE user_id = ?";
+    await util.promisify(connection.query).bind(connection)(deleteQuery, [
+      userId,
+    ]);
+
+    res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error("Failed to delete user:", error);
+    res.status(500).json({ error: "Failed to delete user" });
+  }
+});
 
 //fetch all posts by userId:
 app.get("/posts", (req, res) => {
@@ -187,7 +189,7 @@ app.get("/posts/:id", (req, res) => {
     if (err) {
       console.error("Failed to fetch post:", err);
       res.status(500).json({ error: "Failed to fetch post" });
-    } else {
+    } else if (result.length > 0) {
       const { post_id, user_id, post_title, post_content } = result[0];
       res.json({
         id: post_id,
@@ -195,6 +197,8 @@ app.get("/posts/:id", (req, res) => {
         title: post_title,
         body: post_content,
       });
+    } else {
+      res.status(404).json({ error: "Post not found" });
     }
   });
 });
@@ -232,30 +236,26 @@ app.put("/posts/:id", (req, res) => {
 });
 
 // Delete a post
-app.delete("/posts/:id", (req, res) => {
-  //delete all of the comments on this post
-  const postId = req.params.id;
-  const commentQuery="DELETE FROM comments WHERE post_id = ?";
-  connection.query(commentQuery, [postId], (err, result) => {
-    if (err) {
-      console.error("Failed to delete comments:", err);
-      res.status(500).json({ error: "Failed to delete comments" });
-    } else {
-    }
-  });
-  //delete post
-  const query = "DELETE FROM posts WHERE post_id = ?";
-  connection.query(query, [postId], (err, result) => {
-    if (err) {
-      console.error("Failed to delete post:", err);
-      res.status(500).json({ error: "Failed to delete post" });
-    } else {
-      res.json({ message: "Post deleted successfully" });
-    }
-  });
+app.delete("/posts/:id", async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    // Delete all comments on this post
+    const commentQuery = "DELETE FROM comments WHERE post_id = ?";
+    await util.promisify(connection.query).bind(connection)(commentQuery, [
+      postId,
+    ]);
+
+    // Delete the post
+    const query = "DELETE FROM posts WHERE post_id = ?";
+    await util.promisify(connection.query).bind(connection)(query, [postId]);
+
+    res.json({ message: "Post deleted successfully" });
+  } catch (error) {
+    console.error("Failed to delete post:", error);
+    res.status(500).json({ error: "Failed to delete post" });
+  }
 });
-
-
 
 //fetch all todos by userId:
 app.get("/todos", (req, res) => {
@@ -286,7 +286,7 @@ app.get("/todos/:id", (req, res) => {
     if (err) {
       console.error("Failed to fetch todo:", err);
       res.status(500).json({ error: "Failed to fetch todo" });
-    } else {
+    } else if (result.length > 0) {
       const { todo_id, user_id, todo_description, todo_status } = result[0];
       res.json({
         id: todo_id,
@@ -294,6 +294,8 @@ app.get("/todos/:id", (req, res) => {
         title: todo_description,
         completed: todo_status == "completed",
       });
+    } else {
+      res.status(404).json({ error: "Todo not found" });
     }
   });
 });
@@ -350,7 +352,6 @@ app.delete("/todos/:id", (req, res) => {
   });
 });
 
-
 //fetch all comments by postId:
 app.get("/comments", (req, res) => {
   const postId = req.query.postId;
@@ -366,7 +367,7 @@ app.get("/comments", (req, res) => {
           id: c.comment_id,
           postId: c.post_id,
           body: c.comment_content,
-          name: c.name, 
+          name: c.name,
           email: c.eamil,
         }))
       );
@@ -383,51 +384,52 @@ app.get("/comments/:id", (req, res) => {
     if (err) {
       console.error("Failed to fetch comment:", err);
       res.status(500).json({ error: "Failed to fetch comment" });
-    } else {
+    } else if (result.length > 0) {
       const { post_id, comment_content, name, eamil } = result[0];
       res.json({
         id: commentId,
         postId: post_id,
         body: comment_content,
-        name: name, 
+        name: name,
         email: eamil,
       });
+    } else {
+      res.status(404).json({ error: "Comment not found" });
     }
   });
 });
 
 // Create a new comment
-app.post("/comments", (req, res) => {
-  const { postId, body, userId } = req.body;
-  const query =
-    "INSERT INTO comments (post_id, user_id, comment_content) VALUES (?, ?, ?)";
-  const getUserQuery = "SELECT * FROM users WHERE user_id = ?";
+app.post("/comments", async (req, res) => {
+  try {
+    const { postId, body, userId } = req.body;
+    const query =
+      "INSERT INTO comments (post_id, user_id, comment_content) VALUES (?, ?, ?)";
+    const getUserQuery = "SELECT * FROM users WHERE user_id = ?";
 
-  // Fetch user who posted the comment
-  let user;
-  connection.query(getUserQuery, [userId], (err, result) => {
-    if (err) {
-      console.error("Failed to fetch user:", err);
-      res.status(500).json({ error: "Failed to fetch user" });
-    } else {
-      user = result[0]; // Assuming there is only one user with a given userId
-    }
-  });
+    // Fetch user who posted the comment
+    const [userResult] = await util
+      .promisify(connection.query)
+      .bind(connection)(getUserQuery, [userId]);
+    const user = userResult; // Assuming there is only one user with a given userId
 
-  connection.query(query, [postId, userId, body], (err, result) => {
-    if (err) {
-      console.error("Failed to create comment:", err);
-      res.status(500).json({ error: "Failed to create comment" });
-    } else {
-      res.status(201).json({
-        id: result.insertId,
-        postId,
-        name: user.name, 
-        email: user.eamil,
-        body,
-      });
-    }
-  });
+    // Insert the comment
+    const result = await util.promisify(connection.query).bind(connection)(
+      query,
+      [postId, userId, body]
+    );
+
+    res.status(201).json({
+      id: result.insertId,
+      postId,
+      name: user.name,
+      email: user.email,
+      body,
+    });
+  } catch (error) {
+    console.error("Failed to create comment:", error);
+    res.status(500).json({ error: "Failed to create comment" });
+  }
 });
 
 // Update a comment
